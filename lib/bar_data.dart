ad857f12a624fc78273f3007e8a36f2f0bfe4b2e
jpoh281 <hjp4869@naver.com>
2021-07-26 18:36:44 +0900

    07/26 ADD Documentation

--- lib/bar_data.dart
+++ lib/bar_data.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 
-class BarData {
+class BarData<T> {
+  /// graph value
   final double value;
+
+  /// bottom text
   final String? label;
+
+  /// index
   final int index;
 
-  BarData({required this.value, this.label = '', required this.index});
+  /// If you need Pointed BarData's data, use this.
+  final T? data;
+
+  BarData(
+      {required this.value, this.label = '', required this.index, this.data});
 
   String toString() {
     return "index : $index, value : $value, label : $label";
@@ -13,37 +22,69 @@ class BarData {
 }
 
 class ChartData {
+  /// Selected Bar Color
   final Color selectedColor;
-  final TextStyle selectedTextStyle;
 
+  /// Selected Bar Bottom Text Style
+  final TextStyle? selectedTextStyle;
+
+  /// Unselected Bar Color
   final Color unselectedColor;
-  final TextStyle unselectedTextStyle;
+
+  /// Unselected Bar Bottom Text Style
+  final TextStyle? unselectedTextStyle;
+
+  /// Called when user Scroll chart or Tap Bar
   final ValueChanged<BarData>? onChanged;
+
+  /// Chart's Width
   final double chartWidth;
+
+  /// Chart's Height
+  /// It has 4 + itemBottomHeight + itemHeight
   final double chartHeight;
+
+  /// Chart's Background Color
   final Color chartBackgroundColor;
+
+  /// Chart's Border
   final Border chartBorder;
 
+  /// Bar's strokeWidth
   final double strokeWidth;
+
+  /// Bar Item's itemWidth
   final double itemWidth;
+
+  /// Bar Item's itemHeight
   final double itemHeight;
+
+  /// Bar Label's Height
   final double itemBottomHeight;
+
+  /// Bar Label's Background Color
   final Color itemBottomBackgroundColor;
 
+  /// Bar Item's Count
   final int itemCount;
+
+  /// Chart's scrollController
   final ScrollController scrollController;
 
+  /// Bar Data's maxValue
   final double maxValue;
+
+  /// Bar Data's minValue Default 0
   final double minValue;
 
   ChartData(
       {required this.selectedColor,
-      required this.selectedTextStyle,
+      this.selectedTextStyle,
       required this.maxValue,
-      required this.minValue,
+      this.minValue = 0,
       required this.chartBorder,
       required this.unselectedColor,
-      required this.unselectedTextStyle,
+      this.unselectedTextStyle,
       this.onChanged,
       required this.strokeWidth,
       required this.chartWidth,
